c307e8217d9d19f323e00fc5c9b7ad4d6ce4f014
None Business <none@business.com>
2016-08-14 14:01:10 -0500

    Commit demonstrating queue backup when a consumer is not pulling messages off the queue. Section 4.6.3

--- src/main/java/com/intertech/lab1/Startup.java
+++ src/main/java/com/intertech/lab1/Startup.java
@@ -2,6 +2,9 @@ package com.intertech.lab1;
 
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
 
 /**
  * Created by jayson on 8/14/16.
@@ -11,8 +14,20 @@ public class Startup {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/si-components.xml");
 
-        while (true) {
+//        while (true) {
+//
+//        }
 
-        }
+        MessageChannel channel = context.getBean("messageChannel",MessageChannel.class);
+        Message<String> message1 = MessageBuilder.withPayload("Hello World - one").build();
+        Message<String> message2 = MessageBuilder.withPayload("Hello World - two").build();
+        Message<String> message3 = MessageBuilder.withPayload("Hello World - three").build();
+        System.out.println("Sending message 1");
+        channel.send(message1);
+        System.out.println("Sending message 2");
+        channel.send(message2);
+        System.out.println("Sending message 3");
+        channel.send(message3);
+        System.out.println("Done sending messages");
     }
 }
