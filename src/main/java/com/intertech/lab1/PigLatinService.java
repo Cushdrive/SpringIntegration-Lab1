67d15c8efe525fbf501f455fc0cccb738fc9d300
None Business <none@business.com>
2016-08-20 15:00:44 -0500

    Commit after implementing a synchronous gateway. End of Lab 9 step 5

---
+++ src/main/java/com/intertech/lab1/PigLatinService.java
@@ -0,0 +1,8 @@
+package com.intertech.lab1;
+
+/**
+ * Created by jayson on 8/20/16.
+ */
+public interface PigLatinService {
+    String translate(String english);
+}
--- src/main/java/com/intertech/lab1/Startup.java
+++ src/main/java/com/intertech/lab1/Startup.java
@@ -1,22 +1,23 @@
 package com.intertech.lab1;
 
-
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
-/**
- * Created by jayson on 8/14/16.
- */
 public class Startup {
-    @SuppressWarnings({"resource","unused"})
-    public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/si-components.xml");
 
-        while (true) {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"/META-INF/spring/si-components.xml");
+		
+//		MessageChannel channel = context.getBean("requestChannel", MessageChannel.class);
+//		Message<String> message = MessageBuilder.withPayload("Hello brave new world").build();
+//		channel.send(message);
 
-        }
+        PigLatinService service = context.getBean("latinService",PigLatinService.class);
+        System.out.println(service.translate("Hello brave world."));
 
-    }
+        context.close();
+	}
 }
